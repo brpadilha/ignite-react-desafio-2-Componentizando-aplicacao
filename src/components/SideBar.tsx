2e6748234c421d761fc55d1410ef2394066bdd0d
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2021-03-26 12:21:59 -0300

    desestrutura componentes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import {GenreResponseProps} from '../App'
+type SideBarProps = {
+  genres: Array<GenreResponseProps>;
+  selectedGenreId: number;
+  setSelectedGenreId: (id:number) => void;
+};
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }: SideBarProps) {
+
+  function handleClickButton(id: number){
+    setSelectedGenreId(id);
+  }
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
